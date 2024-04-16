2c2671cc67e0f8507d8eed16c7ecd5f7d4fc1199
alexa <alexa@ALEXANDRAS-LAPT>
2024-04-16 16:11:44 -0500

    Add project files.

---
+++ LCP0417/Program.cs
@@ -0,0 +1,21 @@
+﻿// MIS 3013 001
+// April 17, 2024
+//Alexandra Camarena
+// 113568154
+
+using b;// go to b folder to find definition of Student
+
+Console.WriteLine("List");
+
+List<Student> stuList;
+stuList= new List<Student>();
+
+stuList.Add(new Student());
+
+
+stuList[0].Id = "S101";
+stuList[0].Name = "Tom";
+stuList[0].Grade = 96.6;
+stuList[0].GetLG();// function call
+
+Console.WriteLine(stuList[0].GetInfor());
---
+++ LCP0417/Student.cs
@@ -0,0 +1,41 @@
+﻿namespace b// folder b
+{
+    public class Student
+    {
+        public string Id;
+        public string Name;
+        public double Grade;
+        public char LetterGrade;
+
+        public char GetLG()
+        {
+            if (Grade >= 90)
+            {
+                LetterGrade = 'A';
+            }
+            else if (Grade >= 80)
+            {
+                LetterGrade = 'B';
+            }
+            else if (Grade >= 70)
+            {
+                LetterGrade = 'C';
+            }
+            else if (Grade >= 60)
+            {
+                LetterGrade = 'D';
+            }
+            else
+            {
+                LetterGrade = 'F';
+            }
+            return LetterGrade;
+        }
+
+        public string GetInfor()
+        {
+            return $"ID: {Id}, Name: {Name}, Grade: {Grade} ({LetterGrade})";
+        
+        }
+    }
+}
